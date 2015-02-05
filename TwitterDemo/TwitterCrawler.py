e450fe3c1c041cb368037519b1197f1d08e4dc98
Brenden <btr0005@auburn.edu>
2015-02-04 22:42:14 -0600

    Fixed some minor issues with TwitterCrawler class

--- TwitterDemo/TwitterCrawler.py
+++ TwitterDemo/TwitterCrawler.py
@@ -1,19 +1,22 @@
 import twitter
+import TwitterSearch
 
 class TwitterCrawler(object):
+
 	# Login to api
 	def login(self):
-		api = twitter.Api(consumer_key='684nljJUHfn6SCaYSCG0yAhbW',
+		self.api = twitter.Api(consumer_key='684nljJUHfn6SCaYSCG0yAhbW',
 						consumer_secret='mIRCyxLdIC5cQc7HukUtb7KhKqIvSYOB6LjBZb3CQOQ2n4ents',
 						access_token_key='2805813624-2V4XKmbtM18s8osRDpSsr4H2An7JTpMdBE5N2la',
 						access_token_secret='szChpRZhXg9F7n5gmlQhG2gEXe5C5g1vgYLGfqmeViPj8'
 						)
-		return api
+		return self.api
 	
 	# Search
 	def BasicSearch(self, query):
+		self.query = query
 		search = TwitterSearch.BasicTwitterSearch()
-		tweets = search.search(api, search_term)
+		tweets = search.search(self.api, query)
 		return tweets
 	
 	# SearchLastXDays
@@ -23,7 +26,7 @@ class TwitterCrawler(object):
 	# Output to file
 	def output_tweets(self, tweets):
 		output = open("tweets.txt", "wb")
-		output.write("SEARCH QUERY: " + search_term + "\n")
+		output.write("SEARCH QUERY: " + self.query + "\n")
 
 		for tweet in tweets:
 			output.write("\n\n[" + tweet.api_tweet_data.user.screen_name.encode('utf-8') + "] ")
