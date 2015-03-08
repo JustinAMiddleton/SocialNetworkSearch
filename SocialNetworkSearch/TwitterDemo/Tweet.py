b30c65e807ad6d00d71dc757ef476ed31e41c7a2
WhistleBlower Group <btr@auburn.edu>
2015-03-08 18:24:17 -0500

    Cleaning up production folder

--- SocialNetworkSearch/TwitterDemo/Tweet.py
+++
@@ -1,8 +0,0 @@
-import twitter
-
-class Tweet(object):
-	
-	def __init__(self, api_tweet_data, media, geocode):
-		self.api_tweet_data = api_tweet_data
-		self.media = media
-		self.geocode = geocode
