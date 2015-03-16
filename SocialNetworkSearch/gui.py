2b11e4a0fa22bd033f2eb5e9e26feee3a9de7478
WhistleBlower Group <btr0005@auburn.edu>
2015-03-16 14:57:31 -0500

    Added updates to show results GUI for viewing user posts

--- SocialNetworkSearch/gui.py
+++ SocialNetworkSearch/gui.py
@@ -83,7 +83,7 @@ class App():
 			self.thread.join()
 	
 		self.show_results_window()
-		self.interface.db.close()
+		#self.interface.db.close()
 
 		self.start_button.config(state = NORMAL)
 
@@ -96,14 +96,41 @@ class App():
 		toplevel= Toplevel()
 		toplevel.title('Results')
 		toplevel.focus_set()
-		toplevel.geometry('200x200-160+200')
+		toplevel.geometry('320x320-160+200')
 		results_frame = Frame(toplevel)
 		results_frame.pack()
 
 		top_users = self.thread.results
+		postButtons = []
 		for i in range(0,len(top_users)):
-			user = Label(results_frame, text="[%s] %s" % (str(round(top_users[i]['score'],5)), top_users[i]['username']))
-			user.grid(row=i, column=0, sticky=W)	
+			Label(results_frame, text=i+1).grid(row=i, column=0)
+	
+			user = Label(results_frame, text="[%s] %s" % 
+				(str(round(top_users[i]['score'],5)), top_users[i]['username']))
+			user.grid(row=i, column=1, sticky=W)	
+			
+			postButton = Button(results_frame, text="Show Posts", 
+					command=lambda i=i: self.show_posts(top_users[i]['username']))
+			postButton.grid(row=i, column=2)
+
+	def show_posts(self, username):
+		root = Tk()
+		root.geometry('-160+200')
+		root.title("%s's Posts" % username)
+		scrollbar_x = Scrollbar(root, orient=HORIZONTAL)
+		scrollbar_x.pack(side=BOTTOM, fill=X)
+
+		listbox = Listbox(root, width=50)
+		listbox.pack()
+
+		posts = self.interface.db.get_posts(username)
+
+		for i in range(len(posts)):
+		    listbox.insert(END, posts[i]['content'].encode('utf-8'))
+
+		listbox.config(xscrollcommand=scrollbar_x.set)
+		scrollbar_x.config(command=listbox.xview)
+		
 
 	def define_attribute(self, attribute, index):
 		self.toplevel= Toplevel()
